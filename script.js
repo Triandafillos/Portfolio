61f0db3d5bee3408ec00ee6bdbae64d1aab75237
Triandafillos <georgepapadakis1111@gmail.com>
2025-11-21 17:26:44 -0700

    ease

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelector(".soccer-matches").addEventListener("mouseenter", () => {
+    document.body.style.setProperty("--bgrd-color", "#99FF99")
+})
+
+document.querySelector(".soccer-matches").addEventListener("mouseleave", () => {
+    document.body.style.setProperty("--bgrd-color", "#FFFFFF")
+})
+
+document.querySelector(".capitol-films").addEventListener("mouseenter", () => {
+    document.body.style.setProperty("--bgrd-color", "#FF9999")
+})
+
+document.querySelector(".capitol-films").addEventListener("mouseleave", () => {
+    document.body.style.setProperty("--bgrd-color", "#FFFFFF")
+})
